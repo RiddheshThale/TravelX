7a1aefb0ec892e1ead8e6e60e4c28abf3a183e52
Riddhesh Nivas Thale <riddheshthale@gmail.com>
2026-08-07 11:31:07 +0530

    intitial setup

---
+++ backend/src/main/java/com/travelx/backend/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.travelx.backend.repository;
+
+import com.travelx.backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
---
+++ backend/src/main/java/com/travelx/backend/service/UserService.java
@@ -0,0 +1,19 @@
+package com.travelx.backend.service;
+
+import com.travelx.backend.entity.User;
+import com.travelx.backend.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+  private final UserRepository userRepository;
+
+  public UserService(UserRepository userRepository) {
+    this.userRepository = userRepository;
+  }
+
+  public User registerUser(User user) {
+    return userRepository.save(user);
+  }
+}
